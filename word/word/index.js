ee222365dee02acf680d4f1bb98b8f0148255681
JacksonSignorile <jacksonsignorile139@gmail.com>
2024-04-11 21:57:21 -0500

    word

---
+++ word/word/index.js
@@ -0,0 +1,12 @@
+const frase = prompt("Ingrese una palabra o frase");
+const cantidad = frase.length;
+const mensajeCantidad = frase + " " + cantidad;
+console.log(mensajeCantidad);
+alert(mensajeCantidad)
+
+const fraseMinusculas = frase.toLowerCase();
+const fraseMayusculas = frase.toUpperCase();
+const minmay = fraseMayusculas + " " + fraseMinusculas;
+alert(minmay);
+
+
